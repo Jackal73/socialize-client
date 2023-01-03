591f710cfb650a2a4e2e5c268f543a00643e76c7
Shawn Kebel <fxrender@outlook.com>
2023-01-03 17:33:45 -0600

    :sparkles: Add `Login` page

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,29 @@
+import "./login.scss";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>get S͛OͦCͨIͥAͣLiͥzeͤd</h1>
+          <p>
+            Lorem ipsum dolor sit amen consectetur adipisicing elit. Architect solution sun nam port ea accusation
+            magnitude dolores aqua stadium.
+          </p>
+          <span>Don't have an account?</span>
+          <button>REGISTER</button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form action="">
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button className="">LOGIN</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
