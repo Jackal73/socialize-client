422b4d79974c847c7085e15713243516243ef144
Shawn Kebel <fxrender@outlook.com>
2023-01-07 00:11:38 -0600

    :sparkles: Add `Post` component and styling

---
+++ src/components/posts/Posts.jsx
@@ -0,0 +1,34 @@
+import Post from "../post/Post";
+import "./posts.scss";
+
+  const Posts = () => {
+
+  //TEMPORARY
+  const posts = [
+    {
+      id: 1,
+      name: "John Doe",
+      userId: 1,
+      profilePic:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "Lorem ipsum dolor sit amen consectetur adipisicing elit",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 2,
+      name: "Jane Doe",
+      userId: 2,
+      profilePic:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "Tenant iste voluptuous color rem commodity voluptuous parrot, voluptuous, laboratory consequence enim nostrum cum! Mayor a nam non adaptable minima modi tempore.",
+    },
+  ];
+
+  return <div className="posts">
+    {posts.map(post=>(
+      <Post post={post} key={post.id}/>
+    ))}
+  </div>;
+};
+
+export default Posts
