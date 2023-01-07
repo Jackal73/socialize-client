d8c6aec34010097c5d04bd32413af10a204bbe5c
Shawn Kebel <fxrender@outlook.com>
2023-01-07 00:09:16 -0600

    :sparkles: Add `Stories` component with styling

---
+++ src/components/stories/Stories.jsx
@@ -0,0 +1,50 @@
+import { useContext } from "react";
+import "./stories.scss"
+import { AuthContext } from "../../context/authContext"
+
+const Stories = () => {
+
+  const {currentUser} = useContext(AuthContext)
+
+  //TEMPORARY
+  const stories = [
+    {
+      id: 1,
+      name: "Timmy Toes",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 2,
+      name: "Gladus Sadd",
+      img: "https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      id: 3,
+      name: "Mike Jumpernot",
+      img: "https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      id: 4,
+      name: "Kim Stretcher",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+  ];
+
+  return (
+    <div className="stories">
+      <div className="story">
+          <img src={currentUser.profilePic} alt="" />
+          <span>{currentUser.name}</span>
+          <button>+</button>
+        </div>
+      {stories.map(story=>(
+        <div className="story" key={story.id}>
+          <img src={story.img} alt="" />
+          <span>{story.name}</span>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Stories
